6bbe71cdfb7da8fdfefb3b36a7dfe0825c4ac882
peaceeng <james.peace.eng@outlook.com>
2024-09-20 18:13:10 -0400

    Add titles

--- src/components/Nav/Nav-Brand/Navbrand.js
+++ src/components/Nav/Nav-Brand/Navbrand.js
@@ -5,7 +5,7 @@ const NavBrand = () => {
     return ( 
         <div href="#home" className='navbrand__container'>
            <h1 className='navbrand'>
-               <Link to="/">SHEMA</Link>
+               <Link to="/">RIZIBAR SHOP</Link>
             </h1>
         </div>
      );
