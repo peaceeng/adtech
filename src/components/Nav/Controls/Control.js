cae7e3122ce247cb0c4182dac718e4d7a7c3838a
peaceeng <james.peace.eng@outlook.com>
2024-09-24 10:27:27 -0400

    Add button for login

--- src/components/Nav/Controls/Control.js
+++ src/components/Nav/Controls/Control.js
@@ -1,4 +1,5 @@
 import './Control.css'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import Badge from '@mui/material/Badge';
 import { Link } from 'react-router-dom';
@@ -12,6 +13,11 @@ const Control = () => {
     return ( 
         <div className="control__bar__container">
             <div className="controls__container">
+                <div className="control">
+                    <Link to="/account/login">
+                        <PersonOutlineIcon color="black" size="large" sx={{ width: '35px'}}/>
+                    </Link>
+                </div>
                 <div className="control">
                     <Link to="/wishlist">
                         <Badge badgeContent={wishItems.items.length} color="error">
--- src/components/Nav/Nav-Links/NavLinks.js
+++ src/components/Nav/Nav-Links/NavLinks.js
@@ -4,7 +4,7 @@ import './NavLinks.css'
 const RoutesForCustomer = () => {
     return (
         <ul className="nav">
-            <li className='nav-link'><Link to="/">Home</Link></li> 
+            <li className='nav-link'><Link to="/">Home</Link></li>
             <li className='nav-link'><Link to="/shop">Shop</Link> </li>
             <li className='nav-link'><Link to="/category/men">Men</Link></li> 
             <li className='nav-link'><Link to="/category/women">Women</Link></li> 
