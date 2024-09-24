d0d4b9336450cf8a1f246faf93011aa852889086
peaceeng <james.peace.eng@outlook.com>
2024-09-24 12:59:15 -0400

    Update

--- src/components/Admin/data.js
+++ src/components/Admin/data.js
@@ -1,10 +1,8 @@
 export const feedData = [    
-  { orderNumber: 'RZ2409090152', poNumber: '1042357', totalCharge: '$1052.1', orderDate: '09/05/2024', shippedDate: '09/09/2024', purchaser: 'Ruveyda Ten cate', status: 'Shipped'},
-  { orderNumber: 'RZ2409100235', poNumber: '1042415', totalCharge: '$1006.5', orderDate: '08/31/2024', shippedDate: '09/09/2024', purchaser: 'Nieves Medina', status: 'Shipped' },
-  { orderNumber: 'RZ2409110035', poNumber: '1043148', totalCharge: '$699.9', orderDate: '09/08/2024', shippedDate: '09/11/2024', purchaser: 'Lucy Wheeler', status: 'Shipped'},
-  { orderNumber: 'RZ2409110125', poNumber: '1043200', totalCharge: '$281.7', orderDate: '09/06/2024', shippedDate: '09/11/2024', purchaser: 'Randy Mendoza', status: 'Shipped'},
-  { orderNumber: 'RZ2409120281', poNumber: '1043652', totalCharge: '$521.6', orderDate: '09/08/2024', shippedDate: '09/12/2024', purchaser: 'Sierk Schuurbiers', status: 'Shipped'},
-  { orderNumber: 'RZ2409160031', poNumber: '1044905', totalCharge: '$861.41', orderDate: '09/13/2024', shippedDate: '09/16/2024', purchaser: 'Mike Garrett', status: 'Shipped'},
-  { orderNumber: 'RZ2409180102', poNumber: '1045327', totalCharge: '$443.21', orderDate: '09/13/2024', shippedDate: '09/18/2024', purchaser: 'Tracy Campbell', status: 'Shipped'},
-  { orderNumber: 'RZ2409190079', poNumber: '1046057', totalCharge: '$323.02', orderDate: '09/15/2024', shippedDate: '09/19/2024', purchaser: 'Trinidad Aguilar', status: 'Shipped'},
+  { orderNumber: 'RZ2409120152', poNumber: '1042357', totalCharge: '$2099.7', orderDate: '09/12/2024', shippedDate: '09/16/2024', purchaser: 'Maria Torres', status: 'Shipped'},
+  { orderNumber: 'RZ2409120235', poNumber: '1042415', totalCharge: '$352.2', orderDate: '09/12/2024', shippedDate: '09/16/2024', purchaser: 'Maria Torres', status: 'Shipped' },
+  { orderNumber: 'RZ2409130035', poNumber: '1043148', totalCharge: '$1006.5', orderDate: '09/13/2024', shippedDate: '09/17/2024', purchaser: 'Maria Torres', status: 'Shipped'},
+  { orderNumber: 'RZ2409130125', poNumber: '1043200', totalCharge: '$845.1', orderDate: '09/13/2024', shippedDate: '09/17/2024', purchaser: 'Maria Torres', status: 'Shipped'},
+  { orderNumber: 'RZ2409140281', poNumber: '1043652', totalCharge: '$239.9', orderDate: '09/14/2024', shippedDate: '09/18/2024', purchaser: 'Maria Torres', status: 'Shipped'},
+  { orderNumber: 'RZ2409140031', poNumber: '1044905', totalCharge: '$646.04', orderDate: '09/14/2024', shippedDate: '09/18/2024', purchaser: 'Maria Torres', status: 'Shipped'}
 ]
